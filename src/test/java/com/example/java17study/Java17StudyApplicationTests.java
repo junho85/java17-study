770f0be61f8c881e12fd4f19179287d03b44db9b
june.kim <junho85@gmail.com>
2022-04-22 13:41:31 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/example/java17study/Java17StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java17study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
